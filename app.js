323f1640c6c2aa296f345679b5e3648c6425fbbe
soujanyac <soujanya@yellowmessenger.com>
2020-06-21 02:06:22 +0530

    refactoring and added comments

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
+// Imports
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 // https://try-puppeteer.appspot.com/
+
+//Constants
 let savedToPdf = [];
 const baseUrl = 'http://highscalability.com';
-const baseFilePath = './pdfs';
+const basePdfPath = 'pdfs';
+const baseFilePath = '.';
 const origin = '/blog/category/example';
 const options = {waitUntil: 'networkidle2', timeout: 0};
+const logFileName = 'saved_pdfs.txt';
 
-const getHrefsAndNextPage =  async (page, url, logFilePointer) => {
+//Functions
+const getHrefsAndNextPage = async (page, url, logFilePointer, savedToPdf) => {
     console.log('Page url: ' + url);
     await page.goto(url, options);
-    const hrefs1 = await page.evaluate(
+    const hrefs = await page.evaluate(
         () => Array.from(
             document.querySelectorAll('.journal-entry-navigation-current'),
             a => a.getAttribute('href')
         )
     );
-    for(let i=0;i <hrefs1.length; i++) {
+    for (let i = 0; i < hrefs.length; i++) {
         let newUrl;
-        if(!savedToPdf.includes(hrefs1[i])) {
-            newUrl = `${baseUrl}${hrefs1[i]}`
+        if (!savedToPdf.includes(hrefs[i])) {
+            newUrl = `${baseUrl}${hrefs[i]}`
             await page.goto(newUrl, options);
             console.log('Saving article: ' + newUrl);
-            const filePath = `${baseFilePath}/${hrefs1[i].replace(/\//g, '-').slice(1,hrefs1[i].length).split('.')[0]}`;
+            const filePath = `${baseFilePath}/${basePdfPath}/${hrefs[i].replace(/\//g, '-').slice(1, hrefs[i].length).split('.')[0]}`;
             await page.pdf({
                 path: `${filePath}.pdf`,
                 format: 'A4'
             });
-            logFilePointer.write(hrefs1[i] + '\n');
-            savedToPdf.push(hrefs1[i]);
+            logFilePointer.write(hrefs[i] + '\n');
+            savedToPdf.push(hrefs[i]);
         }
     }
     await page.goto(url, options);
@@ -39,41 +45,56 @@ const getHrefsAndNextPage =  async (page, url, logFilePointer) => {
         )
     );
 };
-
-try {
-    (async () => {
-        let logFilePointer;
-        try {
-            const data = await fs.readFileSync('./saved_pdfs.txt', {encoding: 'utf8', flag: 'r'});
-            console.log('Found saved pdfs already!');
-            savedToPdf = data.split('\n');
-        } catch (e) {
-            savedToPdf = [];
-            if (e.code === 'ENOENT') {
-                console.log('File saved_pdfs.txt not found!');
-                await fs.writeFileSync('./saved_pdfs.txt', '');
-            } else {
-                throw e;
-            }
-        }
-        try {
-            logFilePointer = await fs.createWriteStream('./saved_pdfs.txt', {flags:'a'});
-            console.log('Opened log file...');
-        } catch (e) {
+const getSavedPdfDataFromFile = async () => {
+    let savedToPdf;
+    try {
+        const data = await fs.readFileSync(`${baseFilePath}/${logFileName}`, {encoding: 'utf8', flag: 'r'});
+        console.log('Found saved pdfs already!');
+        savedToPdf = data.split('\n');
+    } catch (e) {
+        savedToPdf = [];
+        if (e.code === 'ENOENT') {
+            console.log('File ' + logFileName + ' not found!');
+            await fs.writeFileSync(`${baseFilePath}/${logFileName}`, '');
+        } else {
             throw e;
         }
+    }
+    return savedToPdf;
+};
+const createPdfFolder = async () => {
+    try {
+        await fs.mkdirSync(`${baseFilePath}/${basePdfPath}`, {recursive: true});
+        console.log('Created folder for pdfs...');
+    } catch (e) {
+        console.log(e.message);
+        throw e;
+    }
+};
+const getLogFilePointer = async () => {
+    try {
+        const logFilePointer = await fs.createWriteStream(`${baseFilePath}/${logFileName}`, {flags: 'a'});
+        console.log('Opened log file...');
+        return logFilePointer;
+    } catch (e) {
+        throw e;
+    }
+};
+
+//Main
+try {
+    (async () => {
         const browser = await puppeteer.launch({headless: true});
         console.log('Launched headless browser...');
         const page = await browser.newPage();
-        try {
-            await fs.mkdirSync('./pdfs', { recursive: true });
-            console.log('Created folder for pdfs...');
-        } catch (e) {
-            console.log(e.message);
-        }
         await page.setDefaultNavigationTimeout(0);
+
+        savedToPdf = await getSavedPdfDataFromFile();
+        await createPdfFolder();
+
         let nextPageLink, url = `${baseUrl}${origin}`;
-        nextPageLink = await getHrefsAndNextPage(page, url, logFilePointer);
+        let logFilePointer = await getLogFilePointer();
+        nextPageLink = await getHrefsAndNextPage(page, url, logFilePointer, savedToPdf);
         while (nextPageLink && nextPageLink[0]) {
             nextPageLink = await getHrefsAndNextPage(page, `${baseUrl + nextPageLink[0]}`, logFilePointer);
         }
