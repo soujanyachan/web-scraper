17134882bc60ec689306100666a530cb1270cf76
soujanyac <soujanya@yellowmessenger.com>
2020-06-21 01:42:21 +0530

    added good logs

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const fs = require('fs');
 let savedToPdf = [];
 const baseUrl = 'http://highscalability.com';
 const baseFilePath = './pdfs';
+const origin = '/blog/category/example';
 const options = {waitUntil: 'networkidle2', timeout: 0};
 
 const getHrefsAndNextPage =  async (page, url, logFilePointer) => {
+    console.log('Page url: ' + url);
     await page.goto(url, options);
     const hrefs1 = await page.evaluate(
         () => Array.from(
@@ -19,6 +21,7 @@ const getHrefsAndNextPage =  async (page, url, logFilePointer) => {
         if(!savedToPdf.includes(hrefs1[i])) {
             newUrl = `${baseUrl}${hrefs1[i]}`
             await page.goto(newUrl, options);
+            console.log('Saving article: ' + newUrl);
             const filePath = `${baseFilePath}/${hrefs1[i].replace(/\//g, '-').slice(1,hrefs1[i].length).split('.')[0]}`;
             await page.pdf({
                 path: `${filePath}.pdf`,
@@ -42,6 +45,7 @@ try {
         let logFilePointer;
         try {
             const data = await fs.readFileSync('./saved_pdfs.txt', {encoding: 'utf8', flag: 'r'});
+            console.log('Found saved pdfs already!');
             savedToPdf = data.split('\n');
         } catch (e) {
             savedToPdf = [];
@@ -53,24 +57,23 @@ try {
             }
         }
         try {
-            logFilePointer = await fs.createWriteStream('./saved_pdfs.txt', {flags:'a'})
+            logFilePointer = await fs.createWriteStream('./saved_pdfs.txt', {flags:'a'});
+            console.log('Opened log file...');
         } catch (e) {
             throw e;
         }
         const browser = await puppeteer.launch({headless: true});
+        console.log('Launched headless browser...');
         const page = await browser.newPage();
         try {
             await fs.mkdirSync('./pdfs', { recursive: true });
+            console.log('Created folder for pdfs...');
         } catch (e) {
             console.log(e.message);
         }
-        const origin = '/blog/category/example';
         await page.setDefaultNavigationTimeout(0);
-        let nextPageLink, url;
-        url = `${baseUrl}${origin}`;
-
+        let nextPageLink, url = `${baseUrl}${origin}`;
         nextPageLink = await getHrefsAndNextPage(page, url, logFilePointer);
-
         while (nextPageLink && nextPageLink[0]) {
             nextPageLink = await getHrefsAndNextPage(page, `${baseUrl + nextPageLink[0]}`, logFilePointer);
         }
